72ee09adaf7e711218c5d3ae2eeccea14cb7eb54
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-05-27 17:03:55 +0600

    Signup and Signin page responsive completed

--- src/components/auth/signin/index.jsx
+++ src/components/auth/signin/index.jsx
@@ -4,10 +4,11 @@ import { BsThreeDots } from "react-icons/bs";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import loginImage from "../../../assets/images/signinBgImage.png";
+import signUpImage from "../../../assets/images/signupBgImage.png";
 import logo from "../../../assets/pirateLogo.svg";
 import GoogleSignIn from "../socials/GoogleSignIn";
 import FacebookSignIn from "../socials/FacebookSignIn";
+import styles from "./signin.module.css";
 
 const SignInPage = () => {
   const [toggle, setToggle] = useState(false);
@@ -18,16 +19,16 @@ const SignInPage = () => {
   } = useForm();
 
   const handleSignIn = (data) => {
-    console.log("Sign In Data:", data);
+    console.log("Sign in Data:", data);
   };
 
   return (
     <div className="bg-black min-h-screen flex justify-center">
       <div className="flex w-full justify-center">
-        <div className="w-[53%] relative">
+        <div className="w-[53%] relative lg:block hidden">
           <Image
             className="w-full h-full object-cover"
-            src={loginImage}
+            src={signUpImage}
             alt="login image"
           />
           <Link href={"/"}>
@@ -37,36 +38,54 @@ const SignInPage = () => {
             &copy; {new Date().getFullYear()} , eSIM Powered by Pirate Mobile
           </p>
         </div>
-        <div className="w-[47%] pr-16 flex flex-col">
-          <div className="2xl:px-20 lg:px-10 py-16">
-            <div className="text-white">
-              <p className="flex items-center gap-3 justify-end">
-                <span>Don't have an account?</span>
-                <Link className="text-[#C09D5E]" href={"/signup"}>
-                  Sign Up
-                </Link>
-                <span className="p-3 border rounded-2xl border-[#C09D5E] bg-[#221f1f] ">
-                  <BsThreeDots className="text-2xl" />
-                </span>
-              </p>
+        <div
+          className={`${styles.backgroundImage} lg:w-[47%] w-full 2xl:pr-16 p-4 flex flex-col`}
+        >
+          <div
+            className={`${styles.contentWrapper} 2xl:px-20 lg:px-10 lg:py-16`}
+          >
+            <div className="flex items-center justify-between lg:justify-end w-full">
+              <div className="lg:hidden block">
+                <Image
+                  className="w-14 h-14"
+                  src={logo}
+                  alt="signup background image"
+                />
+              </div>
+              <div className="text-white">
+                <p className="flex items-center gap-3 justify-end">
+                  <span className="lg:text-xl text-xs">
+                    Don&apos;t have an account?
+                  </span>
+                  <Link
+                    className="text-[#C09D5E] lg:text-xl text-xs"
+                    href={"/signup"}
+                  >
+                    Sign Up
+                  </Link>
+                  <span className="lg:p-3 p-1 border rounded-2xl border-[#C09D5E] bg-[#221f1f] ">
+                    <BsThreeDots className="text-2xl" />
+                  </span>
+                </p>
+              </div>
             </div>
-            <h4 className="text-4xl mt-20 mb-10 text-white font-semibold">
+            <h4 className="lg:text-4xl text-2xl mt-20 mb-10 text-white font-semibold">
               Sign In
             </h4>
-            <div className="flex w-full gap-5 items-center justify-between mb-12">
-              <div className="w-3/5">
+            <div className="flex w-full lg:flex-row flex-col gap-5 items-center justify-between mb-12">
+              <div className="lg:w-7/12 w-full">
                 <GoogleSignIn />
               </div>
-              <div className="w-2/5">
+              <div className="lg:w-5/12 w-full">
                 <FacebookSignIn />
               </div>
             </div>
-            <hr className="w-10 border-[#C0BDCC]" />
-            <p className="text-[#D2D2D2] text-sm mt-5 mb-10">
+            <hr className="w-10 lg:mx-0  mx-auto border-[#C0BDCC]" />
+            <p className="text-[#D2D2D2] text-sm lg:text-start text-center mt-5 mb-10">
               Or sign in using your email address
             </p>
             <form className="w-full" onSubmit={handleSubmit(handleSignIn)}>
-              <div className="flex justify-between gap-4 w-full">
+              <div className="flex justify-between lg:flex-row flex-col gap-4 w-full">
                 <div className="flex flex-col w-full">
                   <label
                     className="text-white text-md font-normal mb-3"
@@ -75,8 +94,8 @@ const SignInPage = () => {
                     Your email
                   </label>
                   <input
-                    className={`bg-[#221f1f] text-white text-lg px-5 py-3 rounded-3xl border ${
-                      errors.email ? "border-red-500" : "border-[#3D3D3D]"
+                    className={`bg-transparent outline-none text-white text-lg px-5 py-3 rounded-3xl border ${
+                      errors.email ? "border-red-500" : "border"
                     }`}
                     type="email"
                     {...register("email", {
@@ -103,8 +122,8 @@ const SignInPage = () => {
                   </label>
                   <div className="relative w-full">
                     <input
-                      className={`bg-[#221f1f] text-white text-lg px-5 w-full py-3 rounded-3xl border ${
-                        errors.password ? "border-red-500" : "border-[#3D3D3D]"
+                      className={`bg-transparent outline-none text-white text-lg px-5 w-full py-3 rounded-3xl border ${
+                        errors.password ? "border-red-500" : "border"
                       }`}
                       type={toggle ? "text" : "password"}
                       {...register("password", {
@@ -139,24 +158,27 @@ const SignInPage = () => {
                 />
                 <label className="text-white" htmlFor="remember">
                   Remember me
+                  <Link
+                    className="text-[#C09D5E] ml-2 hover:underline"
+                    href={"/forget-password"}
+                  >
+                    Forget Password
+                  </Link>
                 </label>
-                <Link
-                  className="text-[#C09D5E] hover:underline"
-                  href={"/forget-password"}
-                >
-                  Forget password
-                </Link>
               </div>
               <div className="mt-10">
                 <button
                   type="submit"
-                  className="text-white font-semibold flex gap-3 items-center bg-[#C09D5E] px-14 rounded-full py-4"
+                  className="text-white font-semibold w-full text-center  bg-[#C09D5E] px-14 rounded-full lg:py-4 py-2"
                 >
-                  <span>Sign In Now</span>
+                  Sign In Now
                 </button>
               </div>
             </form>
           </div>
+          <p className="text-white text-center pt-40 lg:hidden block">
+            &copy; {new Date().getFullYear()} , eSIM Powered by Pirate Mobile
+          </p>
         </div>
       </div>
     </div>
